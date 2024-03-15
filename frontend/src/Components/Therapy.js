8449d107dc912f5b53160043325c6a3849ac2010
Sreeram <SREERAM@LAPTOP-EVHH4L94.>
2024-03-15 23:52:28 +0530

    f

--- frontend/src/Components/Therapy.js
+++ frontend/src/Components/Therapy.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Preloader from './Preloader';
 import { Link } from 'react-router-dom';
+import therapy from '../assets/img/therapy.jpeg'
 
 const Therapy = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       {/* Title */}
       <h1 style={{ fontSize: '2.5rem', marginBottom: '20px' }}>ART THERAPY</h1>
-
       {/* Why Us Section */}
       <section style={{ marginBottom: '30px' }}>
         <h2 style={{ fontSize: '2rem' }}>WHY US??</h2>
-        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <br></br>
+        <div style={{ display: '-ms-grid', justifyContent: 'space-around' }}>
           {/* Theme */}
           <div>
             <h3>Theme</h3>
@@ -34,8 +35,7 @@ const Therapy = () => {
       </section>
 
       {/* Image */}
-      <img src="therapy.jpg" alt="Art Therapy" style={{ width: '500px', marginBottom: '20px' }} />
-
+      <img src={therapy} alt="" style={{width:"50vh", marginBottom:'20px'}}/>
       {/* Forms */}
       <form style={{ marginBottom: '50px' }}>
         <label>User Name</label>
